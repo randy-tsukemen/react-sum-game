a62687d12aaea924cf1d59617d52c9424a8dd0eb
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-08-22 22:10:44 +0900

    rgs3.5

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const PlayNumber = (props) => (
     <button
         className="number"
         style={{ backgroundColor: colors[props.status] }}
-        onClick={() => console.log("Num", props.number)}
+        onClick={() => props.onClick(props.number, props.status)}
     >
         {props.number}
     </button>
@@ -37,6 +37,27 @@ const App = () => {
         return "available";
     };
 
+    const onNumberClick = (number, currentStatus) => {
+        if (currentStatus === "used") {
+            return;
+        }
+        const newCandidateNums =
+            currentStatus === "available"
+                ? candidateNums.concat(number)
+                : candidateNums.filter((cn) => cn !== number);
+
+        if (utils.sum(newCandidateNums) !== stars) {
+            setCandidateNums(newCandidateNums);
+        } else {
+            const newAvailableNums = availableNums.filter(
+                (n) => !newCandidateNums.includes(n)
+            );
+            setStars(utils.randomSumIn(newAvailableNums, 9));
+            setAvailableNums(newAvailableNums);
+            setCandidateNums([]);
+        }
+    };
+
     return (
         <div className="game">
             <div className="help">
@@ -52,6 +73,7 @@ const App = () => {
                             key={numberId}
                             number={numberId}
                             status={numberStatus(numberId)}
+                            onClick={onNumberClick}
                         />
                     ))}
                 </div>
